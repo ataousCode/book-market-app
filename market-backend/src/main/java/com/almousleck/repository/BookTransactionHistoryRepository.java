bdd33a2ed7db616038dfb16fb281410ebcdff42f
ataousCode <microservicestech97@gmail.com>
2024-09-23 16:08:06 +0800

    fixed jpql error

--- market-backend/src/main/java/com/almousleck/repository/BookTransactionHistoryRepository.java
+++ market-backend/src/main/java/com/almousleck/repository/BookTransactionHistoryRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -13,29 +14,23 @@ import java.util.Optional;
 public interface BookTransactionHistoryRepository extends JpaRepository<BookTransactionHistory, Long> {
 
     @Query("""
-            SELECT history
-            FROM BookTransactionHistory history
-            WHERE history.user.id = :userId
+            SELECT
+            (COUNT (*) > 0) AS isBorrowed
+            FROM BookTransactionHistory bookTransactionHistory
+            WHERE bookTransactionHistory.user.id = :userId
+            AND bookTransactionHistory.book.id = :bookId
+            AND bookTransactionHistory.returnApproved = false
           """)
-    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Long id);
+    boolean isAlreadyBorrowedByUser(@Param("bookId") Long bookId, @Param("userId") Long userId);
 
     @Query("""
-            SELECT history
-            FROM BookTransactionHistory history
-            WHERE history.book.owner.id = :userId
-          """)
-    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Long userId);
-
-    @Query("""
-          SELECT
-          (COUNT(*) > 0) AS isBorrowed
-          FROM BookTransactionHistory bookTransactionHistory
-          WHERE bookTransactionHistory.user.id = :userId
-          AND bookTransactionHistory.book.id = :bookId
-          AND bookTransactionHistory.returnApproved = false
-          """)
-    boolean isAlreadyBorrowedByUser(Long bookId, Long userId);
-
+            SELECT
+            (COUNT (*) > 0) AS isBorrowed
+            FROM BookTransactionHistory bookTransactionHistory
+            WHERE bookTransactionHistory.book.id = :bookId
+            AND bookTransactionHistory.returnApproved = false
+            """)
+    boolean isAlreadyBorrowed(@Param("bookId") Integer bookId);
 
     @Query("""
           SELECT transaction
@@ -47,16 +42,6 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
           """)
     Optional<BookTransactionHistory> findBookIdAndUserId(Long bookId, Long userId);
 
-//    @Query("""
-//          SELECT transaction
-//          FROM BookTransactionHistory transaction
-//          WHERE transaction.book.owner.id = :userId
-//          AND transaction.book.id = :bookId
-//          AND transaction.returned = true
-//          AND transaction.returnApproved = false
-//          """)
-//    Optional<BookTransactionHistory> findByBookIdAndUserId(Long bookId, Long userId);
-
     @Query("""
           SELECT transaction
           FROM BookTransactionHistory transaction
@@ -66,6 +51,30 @@ public interface BookTransactionHistoryRepository extends JpaRepository<BookTran
           AND transaction.returnApproved = false
           """)
     Optional<BookTransactionHistory> findByBookIdAndOwnerId(Long bookId, Long userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.user.id = :userId
+          """)
+    Page<BookTransactionHistory> findAllBorrowedBooks(Pageable pageable, Long userId);
+
+    @Query("""
+            SELECT history
+            FROM BookTransactionHistory history
+            WHERE history.book.createBy = :userId
+          """)
+    Page<BookTransactionHistory> findAllReturnedBooks(Pageable pageable, Long userId);
+
+//    @Query("""
+//          SELECT transaction
+//          FROM BookTransactionHistory transaction
+//          WHERE transaction.book.owner.id = :userId
+//          AND transaction.book.id = :bookId
+//          AND transaction.returned = true
+//          AND transaction.returnApproved = false
+//          """)
+//    Optional<BookTransactionHistory> findByBookIdAndUserId(Long bookId, Long userId);
 
 
     //owner_id
